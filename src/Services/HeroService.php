a8acbbf17d325599fe590cf280e084f763031e7c
LPFK <Liliankozlowski36@gmail.com>
2021-07-16 14:57:37 +0200

    Update HeroService.php

--- src/Services/HeroService.php
+++ src/Services/HeroService.php
@@ -13,7 +13,7 @@ class HeroService
         $this->addHero(new Hero('Jonathan','Harris','Mr nobody',true,'Une nulard mais puissant'));
         $this->addHero(new Hero('Brice','Wayne','Batman',false,'une chauve souris pas ouf'));
         $this->addHero(new Hero('Clark','Kent','Superman',false,'Un alien mais qui ressemble a un humain'));
-        $this->addHero(new Hero('Diana','Prince','Wonderwoman',false,'une femme sympatoche.'));
+        $this->addHero(new Hero('Jean-Pierre','Pernaud','JPP',false,'Un homme sombre'));
     }
     function getList()
     {
