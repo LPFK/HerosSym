d283e84f4c867ceef3f84ad152e6be2aa7d45574
LPFK <Liliankozlowski36@gmail.com>
2021-07-16 14:52:55 +0200

    Initial commit

---
+++ src/Services/HeroService.php
@@ -0,0 +1,27 @@
+<?php 
+namespace App\Services;
+
+use App\Entity\Hero;
+
+class HeroService
+{
+    /* initialisation du tableau à vide*/
+    private $_listeHeros = [];
+
+    function __construct()
+    {
+        $this->addHero(new Hero('Jonathan','Harris','Mr nobody',true,'Une nulard mais puissant'));
+        $this->addHero(new Hero('Brice','Wayne','Batman',false,'une chauve souris pas ouf'));
+        $this->addHero(new Hero('Clark','Kent','Superman',false,'Un alien mais qui ressemble a un humain'));
+        $this->addHero(new Hero('Diana','Prince','Wonderwoman',false,'une femme sympatoche.'));
+    }
+    function getList()
+    {
+        return $this->_listeHeros;
+    }
+    function addHero($pHero)
+    {
+        array_push($this->_listeHeros,$pHero);
+    }
+}
+
